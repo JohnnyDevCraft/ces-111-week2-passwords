28e1ccad28ed806fc64699a247434819c5b60a5a
Johnathon Smith <jsmith@autosoftdms.com>
2025-07-14 23:43:10 -0400

    Adds a password strength checker
    Introduces a new feature that checks how strong a password is based on various rules like length and character types.
    
    The program reads from two files to see if the password is common or too weak and gives rules for what makes a good password.
    
    Improves user experience by providing feedback on password strength through a friendly interface.

--- passwords.py
+++ passwords.py
@@ -10,28 +10,71 @@ SPECIAL=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "
  """, ",", ".", "<", ">", "?", "/", "`", "~"]
 
 def word_in_file(word, filename, case_sensitive=False):
-    pass
+    file = open(filename, "r")
+    if not case_sensitive:
+        word = word.lower()
+    for line in file:
+        if not case_sensitive:
+            line = line.lower()
+        if word in line:
+            file.close()
+            return True
+    file.close()
+    return False
 
 def word_has_character(word, character_list):
-    pass
+    for char in character_list:
+        if char in word:
+            return True
+    return False
 
 def word_complexity(word):
-    pass
+    complexity = 0
+    if word_has_character(word, LOWER):
+        complexity += 1
+    if word_has_character(word, UPPER):
+        complexity += 1
+    if word_has_character(word, DIGITS):
+        complexity += 1
+    if word_has_character(word, SPECIAL):
+        complexity += 1
+    return complexity
 
 def password_strength(password, min_length=10, strong_length=16):
-    print(f"Checking password strength for: {password}")
+
+    if word_in_file(password, "wordlist.txt", case_sensitive=False):
+        print("Password is a dictionary word and is not secure.")
+        return 0
+
+    if word_in_file(password, "toppasswords.txt", case_sensitive=True):
+        print("Password is a commonly used password and is not secure.")
+        return 0
+
+    if len(password) < min_length:
+        print("Password is too short and is not secure.")
+        return 1
+
+    if len(password) >= strong_length:
+        print("Password is long, length trumps complexity this is a good password.")
+        return 5
+
+    complexity = word_complexity(password)
+    return complexity + 1
+
+
 
 def main():
-    run = True
-
-    while run:
+    while True:
         print("Password Strength Checker")
         password = input("Enter a password to check its strength, or enter (q/Q) to quit: ")
+        strength = 0
 
         if password.lower() == 'q':
-            run = False
+            exit()
         else:
-            password_strength(password)
+            strength = password_strength(password)
+
+        print(f"Password strength: {strength} (0 = not secure, 5 = very strong)\n")
 
 if __name__ == "__main__":
     main()
